97e56a0a6c0f974c5fda6b2de96d6c2c033ee24f
Pawan kumar <32352500+Pawan0411@users.noreply.github.com>
2018-10-21 23:51:46 +0530

    Update SavedSeats.java

--- app/src/main/java/com/university/soa/bus/SavedSeats.java
+++ app/src/main/java/com/university/soa/bus/SavedSeats.java
@@ -1,14 +1,10 @@
 package com.university.soa.bus;
-
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import java.util.ArrayList;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,14 +20,20 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.university.soa.bus.SeatClass.OnSeatSelected;
-import com.university.soa.bus.SeatClass.SeatSelection;
+import com.university.soa.bus.SeatClass.SelectableAdapter;
+import com.university.soa.bus.SeatClass.TicketActivity;
 
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -40,60 +42,58 @@ import Models.AppStatus;
 import static android.view.View.INVISIBLE;
 import static android.view.View.VISIBLE;
 
-/**
- * Created by pkumar on 5/15/18.
- */
+public class SavedSeats extends AppCompatActivity {
 
-public class SavedSeats extends AppCompatActivity  {
-    AppStatus appStatus;
+
     DatabaseReference ref;
-    FirebaseDatabase Database;
+
+
     Button Saveinfo, button;
-    TextView T1, T2;
     SharedPreferences seats;
     Set<String> selected;
-    BookingInfo info;
-
     List<Integer> selectSeats = new ArrayList<>();
     String str_name, str_empcode, str_psnum, str_phnmber, emp_code,number;
-    EditText Pname, Pnumber, Empcode, passnumber, editText2;
-    private static final String TAG = "PhoneLogin";
-    private boolean mVerificationInProgress = false;
-    private String mVerificationId;
+    EditText Pname, Pnumber, Empcode, passnumber,editText2;
+    BookingInfo info;
+    TextView T1, T2;
+    AppStatus appStatus;
+
+    ;    private String mVerificationId;
     private PhoneAuthProvider.ForceResendingToken mResendToken;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     private FirebaseAuth mAuth;
-
-
+    private boolean mVerificationInProgress = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.booked_info);
-        Database = FirebaseDatabase.getInstance();
-        ref = Database.getReference().child("Booked Details");
-        appStatus = new AppStatus(getApplicationContext());
-        Saveinfo = (Button) findViewById(R.id.saveinfo);
-        button = (Button) findViewById(R.id.button);
-        Pname = (EditText) findViewById(R.id.PName);
-        Pnumber = (EditText) findViewById(R.id.PhnNumber);
-        Empcode = (EditText) findViewById(R.id.EmpCode);
-        passnumber = (EditText) findViewById(R.id.PsNum);
-        editText2 = (EditText) findViewById(R.id.editText2);
-        T1 = (TextView) findViewById(R.id.Opt);
-        T2 = (TextView) findViewById(R.id.Details);
+
         if (getIntent() != null && getIntent().getExtras() != null
                 && getIntent().hasExtra("employee")) {
             emp_code = getIntent().getStringExtra("employee");
-            selectSeats = getIntent().getIntegerArrayListExtra("seats");
+            info = new BookingInfo();
+            info = Parcels.unwrap(getIntent().getParcelableExtra("info"));
+
+
+            selectSeats = info.seats;
         }
 
         selected = new HashSet<>();
-
-
+        appStatus = new AppStatus(getApplicationContext());
+        Saveinfo = findViewById(R.id.saveinfo);
+        button = findViewById(R.id.button);
+        Pname = findViewById(R.id.PName);
+        Pnumber = findViewById(R.id.PhnNumber);
+        Empcode = findViewById(R.id.EmpCode);
+        passnumber = findViewById(R.id.PsNum);
+        editText2 = (EditText) findViewById(R.id.editText2);
+        T1 = (TextView) findViewById(R.id.Opt);
+        T2 = (TextView) findViewById(R.id.Details);
         seats = getSharedPreferences("seats", MODE_PRIVATE);
         selected = seats.getStringSet(emp_code, new HashSet<String>());
         ref = FirebaseDatabase.getInstance().getReference().child("booked details");
+
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -114,7 +114,8 @@ public class SavedSeats extends AppCompatActivity  {
                 Toast.makeText(SavedSeats.this, "Verification Failed", Toast.LENGTH_SHORT).show();
                 if (e instanceof FirebaseAuthInvalidCredentialsException) {
                     // Invalid request
-                    Toast.makeText(SavedSeats.this, "InValid Phone Number", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(SavedSeats.this,
+                            "InValid Phone Number", Toast.LENGTH_SHORT).show();
                     // ...
                 } else if (e instanceof FirebaseTooManyRequestsException) {
                 }
@@ -124,7 +125,7 @@ public class SavedSeats extends AppCompatActivity  {
             @Override
             public void onCodeSent(String verificationId,
                                    PhoneAuthProvider.ForceResendingToken token) {
-                Log.d(TAG, "onCodeSent:" + verificationId);
+                //Log.d(TAG, "onCodeSent:" + verificationId);
                 Toast.makeText(SavedSeats.this, "Verification code has been send on your number", Toast.LENGTH_LONG).show();
                 // Save verification ID and resending token so we can use them later
                 mVerificationId = verificationId;
@@ -136,7 +137,7 @@ public class SavedSeats extends AppCompatActivity  {
         Saveinfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                store();
                 if (appStatus.isOnline()) {
                     str_name = Pname.getText().toString().trim();
                     str_phnmber = Pnumber.getText().toString().trim();
@@ -155,7 +156,6 @@ public class SavedSeats extends AppCompatActivity  {
                                 !str_phnmber.equals(0)) {
                             if (str_empcode.equals("0000")) {
 
-                                /**Add your number here*/
 
                                 number = "9131341690";
                                 PhoneAuthProvider.getInstance().verifyPhoneNumber(
@@ -174,29 +174,26 @@ public class SavedSeats extends AppCompatActivity  {
                                 passnumber.setVisibility(INVISIBLE);
                                 button.setVisibility(VISIBLE);
                                 editText2.setVisibility(VISIBLE);
-                            }
+                            } else if (str_empcode.equals("1234")) {
 
-                            else if (str_empcode.equals("1234")) {
 
-                                    /**Add your number here*/
+                                number = "8462935367";
+                                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                                        "+91 " + number,
+                                        60,
+                                        java.util.concurrent.TimeUnit.SECONDS,
+                                        SavedSeats.this,
+                                        mCallbacks);
 
-                                    number = "8462935367";
-                                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                                            "+91 " + number,
-                                            60,
-                                            java.util.concurrent.TimeUnit.SECONDS,
-                                            SavedSeats.this,
-                                            mCallbacks);
-
-                                    T2.setText("Please Enter the OTP Send to Your Registered Mobile Number " + number);
-                                    T1.setVisibility(INVISIBLE);
-                                    Saveinfo.setVisibility(INVISIBLE);
-                                    Pname.setVisibility(INVISIBLE);
-                                    Pnumber.setVisibility(INVISIBLE);
-                                    Empcode.setVisibility(INVISIBLE);
-                                    passnumber.setVisibility(INVISIBLE);
-                                    button.setVisibility(VISIBLE);
-                                    editText2.setVisibility(VISIBLE);
+                                T2.setText("Please Enter the OTP Send to Your Registered Mobile Number " + number);
+                                T1.setVisibility(INVISIBLE);
+                                Saveinfo.setVisibility(INVISIBLE);
+                                Pname.setVisibility(INVISIBLE);
+                                Pnumber.setVisibility(INVISIBLE);
+                                Empcode.setVisibility(INVISIBLE);
+                                passnumber.setVisibility(INVISIBLE);
+                                button.setVisibility(VISIBLE);
+                                editText2.setVisibility(VISIBLE);
                             } else {
                                 Toast.makeText(SavedSeats.this, "Invalid Employee Code", Toast.LENGTH_SHORT).show();
                             }
@@ -206,22 +203,22 @@ public class SavedSeats extends AppCompatActivity  {
                     }
                 } else {
                     Toast.makeText(getApplicationContext(), "Please see that you have Active internet connection..", Toast.LENGTH_LONG).show();
+
                 }
 
             }
-        });
+            });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, editText2.getText().toString());
                 signInWithPhoneAuthCredential(credential);
-               store();
+                store();
 
             }
-
-
         });
+
 
     }
 
@@ -235,7 +232,6 @@ public class SavedSeats extends AppCompatActivity  {
                             Toast.makeText(SavedSeats.this, "Verification Done", Toast.LENGTH_SHORT).show();
                             Toast.makeText(getApplicationContext(), "Booked Succesfully..", Toast.LENGTH_SHORT).show();
 
-                            // ...
                         } else {
                             // Log.w(TAG, "signInWithCredential:failure", task.getException());
                             if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
@@ -256,11 +252,11 @@ public class SavedSeats extends AppCompatActivity  {
         //Addata ad = new Addata(str_name, str_empcode, str_phnmber, str_psnum);
         //positions1.remove(positions);
         // String b = String.valueOf(seats.getStringSet("s", positions));
-        ref.child(str_empcode).push().setValue(String.valueOf(selectSeats));
+        ref.child(str_empcode).push().setValue(String.valueOf(selected));
         // Toast.makeText(this, "multiple", Toast.LENGTH_SHORT).show();
 
 
-        Toast.makeText(SavedSeats.this, "Seat nos.- "+printSelected(selectSeats), Toast.LENGTH_LONG).show();
+        Toast.makeText(SavedSeats.this, printSelected(selectSeats), Toast.LENGTH_LONG).show();
         Intent intent = new Intent(this, TicketActivity.class);
         info.emp_name = str_name;
         info.emp_code = str_empcode;
@@ -286,9 +282,11 @@ public class SavedSeats extends AppCompatActivity  {
         return result.toString();
     }
 
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
-        finish();
-    }
+
 }
+
+
+
+
+
+
